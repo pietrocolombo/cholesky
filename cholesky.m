7bd99097ecd0ff11f703a0750223921eade8e41f
Pietro Colombo <p.colombo@campus.unimib.it>
2019-04-24 17:47:34 +0200

    aggiunto il calcolo del tempo

--- cholesky.m
+++ cholesky.m
@@ -1,10 +1,11 @@
 
-matrix = importdata('MatriciCalcoloNumerico/ex15.mat');
+matrix = importdata('../MatriciCalcoloNumerico/ex15.mat');
 A = matrix.A;
 size_mat = size(A)
 xe = ones(size_mat(1,1),1);
-b = A*xe;
-
-x = A\b;
-
-norm(x-xe)/norm(xe)
+% per misurare il tempo
+tic;
+b = A * xe;
+x = A \ b;
+timeElapsed = toc
+error = norm(x - xe)/norm(xe)
