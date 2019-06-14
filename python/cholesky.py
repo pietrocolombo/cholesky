51320f7e3df9b4aad359915ef41250e63a81f406
cartantino <c.carta4@campus.unimib.it>
2019-06-14 15:00:17 +0200

    Now python seems to work; re-edited hierarchy of directory

--- python/cholesky.py
+++ python/cholesky.py
@@ -1,105 +1,43 @@
+import os
 import scipy, scipy.io
+
 import csv
 import platform
+import cvxopt, cvxpy
+import numpy
+
 
 from datetime import datetime
-
+from scipy import io, linalg
 from scipy.sparse.linalg import norm, spsolve
 from scipy.linalg import norm
+from cvxopt import cholmod, matrix, sparse
+from cvxpy.interface import matrix_utilities
 
-from scipy import linalg
+names = ["ex15.mat","cfd1.mat","shallow_water1.mat","cfd2.mat","parabolic_fem.mat","apache2.mat","G3_circuit.mat","Flan_1565.mat","StocF-1465.mat"]
 
-#name = raw_input("Enter name Matrix") #per python 2.7 raw_input, per 3+ input
-name = input("Enter name Matrix")
+for i in names:
+	print(i)
+	path = "../../MatriciCalcoloNumerico/" + i 
+	print("starting "+ i +" evaluation\n")
+	start_time = datetime.now()
+	matr = matrix_utilities.sparse2cvxopt(io.loadmat(path)['Problem']['A'][0][0])
+	xe = matrix(numpy.ones([matr.size[0], 1]))
+	b = sparse(matr * xe)
+	x = cholmod.splinsolve(matr,b)
+	t = datetime.now() - start_time
+	print("\nended "+ i +" evaluation")
+	# calcolo l'errore relativo 
+	relative_error = norm(x - xe, 2) / norm(xe, 2)
 
-path = "../../MatriciCalcoloNumericoPy/" + name + ".mtx"
+	print("Writing stats on csv . . !\n")
+	with open('matrix_python.csv','a') as csvFile:
+		#row = f"{name},{n_rows},{mat.nnz},{relative_error}," + \
+   		#        f"{t.seconds}.{t.microseconds},{platform.system()}"
+		row = [i, relative_error, t.seconds, t.microseconds, platform.system()]
+		writer = csv.writer(csvFile)
+		writer.writerow(row)
+	csvFile.close()
 
-path = "../../MatriciCalcoloNumerico/" + name + ".mat"
-print(path)
-
-# carichiamo la matrice serve in array per il metodi linalg.cholesky
-# mat = scipy.io.mmread(path)#.todense()#.toarray()#.tocsc()
-mat = scipy.io.loadmat(path)
-matrix = mat['Problem']['A'][0][0]
-print(matrix)
-use_umfpack = True
-# type of mat scipy.sparse.csc.csc_matrix
-# https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csc_matrix.html
-
-#n_rows, n_columns = A.shape
-#p_non_zero = mat.nnz / (n_rows * n_columns)
-n_rows, n_columns = matrix.shape
-xe = scipy.ones(n_rows)
-
-start_time = datetime.now()
-# per il calcolo di b non va bene che sia array perciò la ricarico 
-#b = scipy.io.mmread(path).tocsc() * xe
-b = matrix * xe
-print('prima')
-# fattorizzazione di cholesky ottendo una matrice triangolare
-L = linalg.cholesky(mat, lower=True)
-#(c, L) = linalg.cho_factor(mat, lower = True)
-print('dopo')
-#(c, L2) = linalg.cho_factor(mat, lower = False)
-print(b.shape)
-# usi la matrice L per risolvere il sistema dato b
-x = linalg.cho_solve((L,True), b)
-#x = spsolve(mat, b, use_umfpack = use_umfpack)
-print(x.shape)
-#L2 = linalg.cholesky(mat, lower=False)
-#x = linalg.solve_triangular(L, b, lower=True)
-#A2 = L * L2
-#relative_error = norm(mat - A2, 2) / norm(A2, 2)
-print(xe)
-#relative_error = mat - A2
-t = datetime.now() - start_time
-print("prova")
-# calcolo l'errore relativo 
-relative_error = norm(x - xe, 2) / norm(xe, 2)
-
-print("finished!\n")
-with open('matrix_py_false.csv','a') as csvFile:
-	#row = f"{name},{n_rows},{mat.nnz},{relative_error}," + \
-   	#        f"{t.seconds}.{t.microseconds},{platform.system()}"
-	row = [name, n_rows,n_columns, 0, relative_error, t.seconds, t.microseconds, platform.system()]
-	writer = csv.writer(csvFile)
-	writer.writerow(row)
-
-#if use_umfpack == False:
-#	print("Evaluating\n...")
-#	#maybe this is the right format in wich the matrix has to be saved as; https://docs.scipy.org/doc/scipy/reference/sparse.html
-#	x = spsolve(mat, b, use_umfpack = use_umfpack)
-#	#x = spsolve(mat, b)
-#	t = datetime.now() - start_time
-#
-#	relative_error = norm(x - xe, 2) / norm(xe, 2)
-#	print("finished!\n")
-#	with open('matrix_py_false.csv','a') as csvFile:
-#    	#row = f"{name},{n_rows},{mat.nnz},{relative_error}," + \
-#    	#        f"{t.seconds}.{t.microseconds},{platform.system()}"
-#		row = [name, n_rows,n_columns, mat.nnz, relative_error, t.seconds, t.microseconds, platform.system()]
-#		writer = csv.writer(csvFile)
-#		writer.writerow(row)
-#else:
-#	#maybe this is the right format in wich the matrix has to be saved as; https://docs.scipy.org/doc/scipy/reference/sparse.html
-#	print("Evaluating\n...")
-#	x = spsolve(mat, b, use_umfpack = use_umfpack)
-#	#x = spsolve(mat, b)
-#	t = datetime.now() - start_time
-#
-#	relative_error = norm(x - xe, 2) / norm(xe, 2)
-#	print("finished!")
-#	with open('matrix_py_true.csv','a') as csvFile:
-#    	#row = f"{name},{n_rows},{mat.nnz},{relative_error}," + \
-#    	#        f"{t.seconds}.{t.microseconds},{platform.system()}"
-#		row = [name, n_rows,n_columns, mat.nnz, relative_error, t.seconds, t.microseconds, platform.system()]
-#		writer = csv.writer(csvFile)
-#		writer.writerow(row)
-
-
-csvFile.close()
-
-print(t)
-print(relative_error)
 
 print ("End")
