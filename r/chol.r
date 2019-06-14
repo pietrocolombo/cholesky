305a2e6a0171dea964b5b340edcc4b121ba5fcb8
Pietro Colombo <pietrocolombo@me.com>
2019-06-14 18:43:07 +0200

    ho aggiunto in R la parte di incemento della memoria che poi salvo nel csv

--- r/chol.r
+++ r/chol.r
@@ -15,6 +15,11 @@ if(!require(optimbase)){
  if(!require(tictoc)){
     install.packages("tictoc")
     library("tictoc")
+ }
+
+if(!require(profmem)){
+   install.packages("profmem")
+   library("profmem")
 }
 
 #names <- c('ex15.mtx','cfd1.mtx','shallow_water1.mtx','cfd2.mtx','parabolic_fem.mtx','apache2.mtx','G3_circuit.mtx','Flan_1565.mtx','StocF-1465.mtx')
@@ -23,22 +28,23 @@ mat = args[6]
 #for (mat in names){
    path <- paste('../../MatriciCalcoloNumericoPy', mat, sep = "/")
    print(path)
-   tic()
-   matrix <- readMM(path)
+   memory <- profmem({
+      tic()
+      matrix <- readMM(path)
    
-   size_mat <- dim(matrix)[1]
-   xe <- ones(size_mat,1)
+      size_mat <- dim(matrix)[1]
+      xe <- ones(size_mat,1)
    
-   tic()
-   b <- matrix %*% xe
+      tic()
+      b <- matrix %*% xe
    
-   A.chol <- Cholesky(matrix)
-   x <- solve(A.chol, b)
-   timeElapsed <- toc()
+      A.chol <- Cholesky(matrix)
+      x <- solve(A.chol, b)
+      timeElapsed <- toc()
+   })
+      error <- norm(x - xe, '2')/norm(xe, '2')
    
-   error <- norm(x - xe, '2')/norm(xe, '2')
-   
-   csv_write <- data.frame(mat, error, timeElapsed$toc-timeElapsed$tic, .Platform$OS.type)
+   csv_write <- data.frame(mat, error, timeElapsed$toc-timeElapsed$tic, sum(memory$bytes), .Platform$OS.type)
    if(!file.exists('data.csv')){
       write.table(csv_write,file="data.csv", append=TRUE,sep=",",row.names=FALSE) 
       first_time = FALSE
